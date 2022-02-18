3576502719411246a93872d69e3a6dc63e0f09a2
TeamProject12COSC <99223048+TeamProject12COSC@users.noreply.github.com>
2022-02-18 13:10:58 -0600

    Joshua QuoteForm.js

--- Assignment2/QuoteForm.js
+++ Assignment2/QuoteForm.js
@@ -2,4 +2,3 @@ function myFunction() {
     document.getElementById('datepicker').valueAsDate = new Date();
     document.getElementById('datepicker').min = new Date().toISOString().substring(0, 10);
   }
-  
