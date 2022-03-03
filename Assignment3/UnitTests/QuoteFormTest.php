b3f964d91696d04e06648a1cf181da51a28f446d
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-03 17:57:08 -0600

    Joshua QuoteFormTest.php

---
+++ Assignment3/UnitTests/QuoteFormTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class QuoteFormTest extends TestCase
+{
+    
+    public function testInputs(): void
+    {
+        $_POST["gallons"] = (mt_rand(0, 999999) / 10);
+        $_POST["datepicker"] = "2022-09-04";
+        require_once "QuoteForm.php";
+
+        $this->assertNotNull($array);
+        $this->assertNotNull($date);
+        $this->assertEquals($date, "2022-09-04");
+
+        $this->assertGreaterThan(0, $gallonPrice);
+        $this->assertGreaterThan(0, $gal);
+        $this->assertEquals($gal, $_POST["gallons"]);
+        $this->assertGreaterThan(0, $total);
+    
+    }
+
+
+
+
+}
+
+?>
