6db6e54439e143cf05128b4483c2497418eed565
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-03 20:03:28 -0600

    Joshua added more unit tests

--- Assignment3/UnitTests/QuoteFormTest.php
+++ Assignment3/UnitTests/QuoteFormTest.php
@@ -1,30 +1,38 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+require_once 'vendor/autoload.php';
 
 final class QuoteFormTest extends TestCase
 {
-    
     public function testInputs(): void
     {
-        $_POST["gallons"] = (mt_rand(0, 999999) / 10);
-        $_POST["datepicker"] = "2022-09-04";
+        $faker = Faker\Factory::create();
+        $addressHolder = $faker->address;
+        $dateHolder = $faker->date($format = 'Y-m-d');
+        $_POST["gallons"] = $faker->randomFloat($nbMaxDecimals = 0, $min = 1, $max = 10000000);
+        $_POST["datepicker"] = $dateHolder;
+        $_POST["deliveryaddress"] = $addressHolder;
+        $_POST["suggestedprice"] = $faker->randomFloat($nbMaxDecimals = 0, $min = 1, $max = 10000000);
+        $_POST["total"] = $faker->randomFloat($nbMaxDecimals = 0, $min = 1, $max = 10000000);
         require_once "QuoteForm.php";
 
+        $this->assertNotNull($addressHolder);
+        $this->assertNotNull($deliveryAddress);
         $this->assertNotNull($address);
+        $this->assertNotNull($suggestedPrice);
+        $this->assertNotNull($totalPrice);
+        $this->assertNotNull($dateHolder);
         $this->assertNotNull($date);
-        $this->assertEquals($date, "2022-09-04");
+
+        $this->assertEquals($dateHolder, $_POST["datepicker"]);
+        $this->assertEquals($addressHolder, $_POST["deliveryaddress"]);
+        $this->assertEquals($gal, $_POST["gallons"]);
+        $this->assertEquals($gallonPrice * $gal, $total);
 
         $this->assertGreaterThan(0, $gallonPrice);
         $this->assertGreaterThan(0, $gal);
-        $this->assertEquals($gal, $_POST["gallons"]);
-        $this->assertGreaterThan(0, $total);
-    
     }
-
-
-
-
 }
 
 ?>
