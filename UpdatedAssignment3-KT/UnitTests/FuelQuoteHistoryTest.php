2cc549a3b94418e97610ae55398335c69f1bf775
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-12 15:36:28 -0600

    Joshua FuelQuoteHistoryTest.php

--- UpdatedAssignment3-KT/UnitTests/FuelQuoteHistoryTest.php
+++ UpdatedAssignment3-KT/UnitTests/FuelQuoteHistoryTest.php
@@ -13,17 +13,14 @@ final class FuelQuoteHistoryTest extends TestCase
         for ($i = 0; $i < $loopNum; $i++)
         {   
             $this->assertGreaterThan(0, $arrayGal[$i]);
-            $this->assertIsInt($arrayGal[$i], "Value is not integer");
+            $this->assertIsFloat($arrayGal[$i], "Value is not float");
             $this->assertGreaterThan(0, $arrayTotal[$i]);
             $this->assertIsFloat($arrayTotal[$i], "value is not a float");
             $this->assertNotNull($arrayDate[$i]);
-            $this->assertInternalType('string', $arrayDate[$i]);
-
         }
         $this->assertGreaterThan(0, $gallonPrice);
         $this->assertIsFloat($gallonPrice, "value is not a float");
         $this->assertNotNull($address);
-        $this->assertInternalType('string', $address);
 
 
 
