e58e69c9347d99ae66b8e4fdfbe6f3e902ced44e
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-13 14:29:57 -0500

    Joshua

--- UpdatedAssignment3-KT/QuoteForm.js
+++ UpdatedAssignment3-KT/QuoteForm.js
@@ -2,7 +2,7 @@ function myFunction() { //set minimum date and also default date to current day
     document.getElementById('datepicker').valueAsDate = new Date();
     document.getElementById('datepicker').min = new Date().toISOString().substring(0, 10);
   }
-
+ 
   function setDateMin() {
     document.getElementById('datepicker').min = new Date().toISOString().substring(0, 10);
   }
