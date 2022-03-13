9a68696b7b0e128a92f7a459f5442633e13d7fdd
Ryan Hoang <32445507+3ll0@users.noreply.github.com>
2022-03-12 19:29:46 -0600

    Update Profile.php

--- Assignment3/Profile.php
+++ Assignment3/Profile.php
@@ -18,7 +18,66 @@ class Profile {
         $this->state = $state;
         $this->zipCode = $zipCode;
     }
-
+    function isValid() [
+        $valid = true;
+        if(empty($firstName)) {
+            echo $firstName . ' is empty\n';
+            $valid = false;
+        }
+        if(strlen($firstName) > 50) {
+            echo $firstName . ' is too long\n';
+            $valid = false;
+        }
+        if(empty($lastName)) {
+            echo $lastName . ' is empty\n';
+            $valid = false;
+        }
+        if(strlen($lastName) > 50) {
+            echo $lastName . ' is too long\n';
+            $valid = false;
+        }
+        if(empty($address1)) {
+            echo $address1 . ' is empty\n';
+            $valid = false;
+        }
+        if(strlen($address1) > 100) {
+            echo $address1 . ' is too long\n';
+            $valid = false;
+        }
+        if(empty($address2)) {
+            echo $address2 . ' is empty\n';
+            $valid = false;
+        }
+        if(strlen($address2) > 100) {
+            echo $address2 . ' is too long\n';
+            $valid = false;
+        }
+        if(empty($city)) {
+            echo $city . ' is empty\n';
+            $valid = false;
+        }
+        if(strlen($city) > 100) {
+            echo $city . ' is too long\n';
+            $valid = false;
+        }
+        if(empty($state)) {
+            echo $state . ' is empty\n';
+            $valid = false;
+        }
+        if(empty($zipCode)) {
+            echo $zipCode . ' is empty\n';
+            $valid = false;
+        }
+        if(strlen($zipCode) < 5) {
+            echo $zipCode . ' is not long enough\n';
+            $valid = false;
+        }
+        if(strlen($zipCode) > 9) {
+            echo $zipCode . ' is too long\n';
+            $valid = false;
+        }
+        return true;
+    }
     function getFirstName() {
         return $this->firstName;
     }
