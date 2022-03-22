0f9881b847320c2d936c00633f0e9b5ad58d158b
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-22 14:15:37 -0500

    Create Database, then create tables

--- Assignment4/config.php
+++ Assignment4/config.php
@@ -3,12 +3,51 @@
 $server = "localhost";
 $user = "root";
 $pass = "";
-$db = "userlogins";
+
+$conn = mysqli_connect($server, $user, $pass);
+
+if(!$conn){
+    die("<script>alert('Failed to connect to server')</script>");
+}
+
+// Create database
+$db_selected = $conn->select_db('myDB');
+if (!$db_selected)
+{
+   $sql = "CREATE DATABASE myDB";
+   if ($conn->query($sql) === TRUE) {
+    // echo "Database created successfully";
+   } else {
+   //  echo "Error creating database: " . $conn->error;
+   }
+}
+$conn->close();
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "myDB";
 
 $conn = mysqli_connect($server, $user, $pass, $db);
 
 if(!$conn){
     die("<script>alert('Failed to connect to server')</script>");
 }
+   // sql to create FuelQuote Table
+   $sql = "CREATE TABLE FuelQuote (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    gallonsRequested INT(255) NOT NULL,
+    deliveryAddress VARCHAR(100) NOT NULL,
+    deliveryDate DATE NOT NULL,
+    dollarsPerGallon DOUBLE(255, 2) UNSIGNED  NOT NULL,
+    totalDue DOUBLE(255, 2) UNSIGNED  NOT NULL
+    )";
+    if ($conn->query($sql) === TRUE) {
+      //echo "Table created successfully";
+    } else {
+      //echo "Error creating table: " . $conn->error;
+    }
 
+    $conn->close();
 ?>
