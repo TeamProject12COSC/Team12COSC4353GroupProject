d60b129c01d70f5717a58bf927ea8092f4119903
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-22 14:29:32 -0500

    Fixed zipcode checker

--- Assignment4/Signup.js
+++ Assignment4/Signup.js
@@ -1,3 +1,13 @@
 function checkZipNum() {
-  return !isNaN(document.forms["profile"]["Zipcode"]);
+
+  let input = document.forms["profile"]["Zipcode"].value;
+  let valid = true;
+  for (let i = 0; i < input.length; i++)
+  {
+    if (isNaN(input[i]))
+    {
+      alert("Only Digits 0-9 Allowed");
+      return false;
+    }
+  }
 }
