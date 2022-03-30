2a7bac4c6af776b8133402dd84fa66310bbeb7dd
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-03-30 15:24:05 -0500

    Big Int instead of int

--- Assignment4/config.php
+++ Assignment4/config.php
@@ -37,7 +37,7 @@ if(!$conn){
    $sql = "CREATE TABLE FuelQuote (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
-    gallonsRequested INT(255) NOT NULL,
+    gallonsRequested BIGINT NOT NULL,
     deliveryAddress VARCHAR(100) NOT NULL,
     deliveryDate DATE NOT NULL,
     dollarsPerGallon DOUBLE(255, 2) UNSIGNED  NOT NULL,
