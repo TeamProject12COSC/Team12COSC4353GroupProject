2fb9f3f4b06b426a51e4b498336ca91f90830d80
TeamProject12COSC <64809002+JoshuaCyber@users.noreply.github.com>
2022-04-01 19:27:05 -0500

    Joshua

--- Assignment4/FuelQuoteHistory.php
+++ Assignment4/FuelQuoteHistory.php
@@ -1,26 +1,7 @@
 <?php 
    session_start(); //start session
    require_once 'vendor/autoload.php';
-   $faker = Faker\Factory::create();
-   //connect to database and retrieve history data for user
 
-
-   //connect to database and retrieve history data for user
- //   $arrayGal = array();
-//    $arrayTotal = array();
- //   $arrayDate = array();
- //   $gallonPrice =  $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 10);
-
-  //  $loopNum = $faker->numberBetween(0, 100);
-   // for ($i = 0; $i < $loopNum; $i++)
-  //  {
-   //     $value =  $faker->randomFloat($nbMaxDecimals = 0, $min = 1, $max = 10000000);
-   //     $arrayGal[] = $value;
-    //    $arrayTotal[] = $gallonPrice * $value;
-  //      $arrayDate[] = $faker->date($format = 'm-d-Y');
-  //  }
-
- //   $address = $faker->address;
 
 ?>
 
