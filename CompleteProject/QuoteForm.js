205656685be3fc48a233fca51a52612e964b457e
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-04-07 13:00:58 -0500

    Joshua QuoteForm disabling buttons based on input

--- CompleteProject/QuoteForm.js
+++ CompleteProject/QuoteForm.js
@@ -1,4 +1,52 @@
   function setDateMin() {
     document.getElementById('datepicker').min = new Date().toISOString().substring(0, 10);
   }
+
+
+window.onload = function() {
+  if (document.getElementById("gallons").value == "" || document.getElementById("datepicker").value == "")
+  {
+     document.getElementById("getQuote").disabled = true;
+  }
+  if (document.getElementById("gallons").value == "" || document.getElementById("datepicker").value == "")
+  {
+    document.getElementById("submit").disabled = true;
+  }
+  document.getElementById("gallons").addEventListener("input", listen);
+  document.getElementById("datepicker").addEventListener("input", listen);
+
+  document.getElementById("total").addEventListener("input", listenSubmit);
+  document.getElementById("suggestedprice").addEventListener("input", listenSubmit);
+
+  document.getElementById("gallons").addEventListener("input", changed);
+  document.getElementById("datepicker").addEventListener("input", changed);
+  }
  
+function listen() {
+  let gallon = document.getElementById("gallons");  
+  let date = document.getElementById("datepicker");
+  if (gallon.value != "" && date.value != "")
+    {
+      document.getElementById("getQuote").disabled = false;
+    }
+    else
+    {
+      document.getElementById("getQuote").disabled = true;
+    }
+  
+}
+
+function listenSubmit() {
+  let suggested = document.getElementById("suggestedprice");
+  let total = document.getElementById("total");
+
+  if (suggested.value != "" && total.value != "")
+  {
+    document.getElementById("submit").disabled = false;
+    
+  }
+}
+
+function changed() {
+  document.getElementById("submit").disabled = true;
+}
