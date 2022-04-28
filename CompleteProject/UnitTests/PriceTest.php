6af9c791cdc192f0c378f0828403ec8eab15a3f0
Joshua <64809002+JoshuaCyber@users.noreply.github.com>
2022-04-28 14:32:31 -0500

    Joshua Unit Test

--- CompleteProject/UnitTests/PriceTest.php
+++ CompleteProject/UnitTests/PriceTest.php
@@ -12,6 +12,34 @@ final class PriceTest extends TestCase
         $price = new Price(0.02, 0.01, 1500, 0.02);
         $this->assertEquals($price->calculateTotal(), 2542.5);
         $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.04, 0, 1500, 0.02);
+        $this->assertEquals($price->calculateTotal(), 2610);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.04, 0.01, 1500, 0.02);
+        $this->assertEquals($price->calculateTotal(), 2587.5);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.02, 0, 1500, 0.02);
+        $this->assertEquals($price->calculateTotal(), 2565);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.04, 0, 900, 0.03);
+        $this->assertEquals($price->calculateTotal(), 1579.5);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.04, 0.01, 900, 0.03);
+        $this->assertEquals($price->calculateTotal(), 1566);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.02, 0.01, 900, 0.03);
+        $this->assertEquals($price->calculateTotal(), 1539);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
+
+        $price = new Price(0.02, 0, 900, 0.03);
+        $this->assertEquals($price->calculateTotal(), 1552.5);
+        $this->assertIsNumeric($price->calculateTotal(), "This value is not numeric");
     }
 
 }
